11bea932d727bfa121648484952f5a23118d5632
Churchil-Oduor <churchilokechoduor>
2024-09-06 15:44:11 +0300

    cleaned the target file

--- rust_basics/tests/adder/target/debug/deps/adder-ef96384b0655776f.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Churchil\Projects\Getting-Rusty\rust_basics\tests\adder\target\debug\deps\libadder-ef96384b0655776f.rmeta: src\lib.rs
-
-C:\Users\Churchil\Projects\Getting-Rusty\rust_basics\tests\adder\target\debug\deps\libadder-ef96384b0655776f.rlib: src\lib.rs
-
-C:\Users\Churchil\Projects\Getting-Rusty\rust_basics\tests\adder\target\debug\deps\adder-ef96384b0655776f.d: src\lib.rs
-
-src\lib.rs:
